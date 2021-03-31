43c33f3ed1f0ff8b0fba8c69b5d7d9508a525721
Willem Scholten <vexteacher@gmail.com>
2021-03-31 17:02:36 -0400

    Fixed gearset mismatch in globals

--- src/globals.cpp
+++ src/globals.cpp
@@ -13,8 +13,8 @@
 
 // declare the motor objects we are using - see initialize section for more
 // details see the initialize() function in main.cpp
-pros::Motor left_wheel (LEFT_MOTOR_PORT, MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor right_wheel (RIGHT_MOTOR_PORT, MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor left_wheel (LEFT_MOTOR_PORT, MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor right_wheel (RIGHT_MOTOR_PORT, MOTOR_GEARSET_36, true, pros::E_MOTOR_ENCODER_DEGREES);
 
 int motorMaxSpeed = 50;            // Maximum movement speed
 const int motorDefaultSpeed = 75;  // our default movement speed
